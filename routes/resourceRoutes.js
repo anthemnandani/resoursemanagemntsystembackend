b01f451b995959e4c10e92d9636f907358873bbe
Sandeep-anthem <sandeep.b@antheminfotech.com>
2025-04-10 11:58:50 +0530

    pdf working with image in url issue

--- routes/resourceRoutes.js
+++ routes/resourceRoutes.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const upload = require("../config/multer"); 
+const express = require("express");
+const upload = require("../config/multer");
 const {
   createResource,
   getAllResources,
@@ -9,17 +9,24 @@ const {
 } = require("../controllers/resourceController");
 
 const router = express.Router();
-router.post('/createresourse', upload.fields([
-  { name: "images", maxCount: 5 },
-  { name: "documents", maxCount: 5 }
-])
-, createResource);
-router.get('/', getAllResources);
-router.get('/getAvaliableResources', getAvaliableResources);
-router.put('/updateresourse/:id', upload.fields([
-  { name: "images", maxCount: 10 },
-  { name: "documents", maxCount: 5 },
-]), updateResource);
-router.delete('/deleteresourse/:id', deleteResource);
+router.post(
+  "/createresourse",
+  upload.fields([
+    { name: "images", maxCount: 5 },
+    { name: "documents", maxCount: 5 },
+  ]),
+  createResource
+);
+router.get("/", getAllResources);
+router.get("/getAvaliableResources", getAvaliableResources);
+router.put(
+  "/updateresourse/:id",
+  upload.fields([
+    { name: "images", maxCount: 5 },
+    { name: "documents", maxCount: 5 },
+  ]),
+  updateResource
+);
+router.delete("/deleteresourse/:id", deleteResource);
 
 module.exports = router;
--- utils/uploadToCloudinary.js
+++ utils/uploadToCloudinary.js
@@ -1,17 +1,15 @@
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 
-const uploadToCloudinary = (fileBuffer, folder, isVideo = false) => {
+const uploadToCloudinary = (fileBuffer, folder) => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
       {
         folder,
-        resource_type: isVideo ? "video" : "image", // Handle video or image
+        resource_type: "auto",
       },
       (error, result) => {
-        if (error) {
-          return reject(error);
-        }
+        if (error) return reject(error);
         resolve(result);
       }
     );
